bd670b90c92b703d826062006d9d0fc50b0b3164
kristinbaker <kristinbaker720@gmail.com>
2021-08-18 13:55:01 -0500

    Created src folder

---
+++ src/style.css
@@ -0,0 +1,169 @@
+h1 {
+  text-align: center;
+  margin-top: 20px;
+  padding-top: 20px;
+  display: block;
+  font-size: 80px;
+}
+
+.container {
+  max-width: 600px;
+  margin-top: 0px;
+  padding-bottom: 100px;
+  padding-left: 25px;
+  padding-right: 25px;
+  background-color: #7db5ac;
+  border-radius: 12px;
+}
+
+.rotate {
+  transform: rotate(220deg);
+  padding: 21px 8px;
+  margin: 0 auto;
+  font-size: 18px;
+  display: inline-block;
+  color: #fff;
+  float: left;
+}
+
+.containerWeather {
+  max-width: 300px;
+  margin: 0 auto;
+  margin-top: 60px;
+  margin-bottom: 70px;
+  padding-bottom: 40px;
+  padding-top: 0px;
+  background-color: rgba(255, 255, 255, 0.3);
+  border-radius: 12px;
+}
+
+.currentWeather {
+  text-align: center;
+  color: white;
+  font-size: 25px;
+  margin-top: 10px;
+}
+
+.weeklyForecast {
+  display: inline-block;
+  padding: 30px;
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 12px;
+  width: 105px;
+}
+
+.daysContainer {
+  text-align: center;
+}
+
+.todaysHighAndLow {
+  color: white;
+  margin-inline-start: 25%;
+  margin-inline-end: 25%;
+  text-align: center;
+  font-size: 25px;
+}
+.daysOfTheWeek {
+  color: #fff;
+  text-align: center;
+  font-size: 25px;
+}
+
+.emojisOfWeather {
+  color: #fff;
+  text-align: center;
+  font-size: 25px;
+}
+
+.highsOfTheWeek {
+  color: #fff;
+  text-align: center;
+}
+
+.lowsOfTheWeek {
+  color: #fff;
+  text-align: center;
+}
+
+.currentDateTime {
+  text-align: right;
+  color: white;
+  font-size: 20px;
+  padding-top: 25px;
+  display: block;
+  float: right;
+}
+
+#location-button {
+  background: none;
+  border: none;
+  color: white;
+}
+
+#first-break {
+  margin-inline-start: 30%;
+  margin-inline-end: 30%;
+  color: white;
+  font-weight: bolder;
+  opacity: 100%;
+}
+
+#current-location {
+  float: left;
+  color: #fff;
+  font-size: 30px;
+  margin-bottom: 0px;
+  display: inline-block;
+  padding-top: 25px;
+}
+
+#temp-measure {
+  padding-top: 0px;
+  display: inline-block;
+}
+#celsius-link {
+  text-decoration: none;
+  color: white;
+}
+
+#celsius-link:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+#fahrenheit-link {
+  text-decoration: none;
+  color: white;
+}
+
+#fahrenheit-link:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+#current-temp {
+  text-align: center;
+  color: white;
+  font-size: 45px;
+}
+
+#todays-rain-wind {
+  color: white;
+  margin-inline-start: 15%;
+  margin-inline-end: 15%;
+  text-align: center;
+  font-size: 15px;
+  padding-top: 10px;
+}
+
+#search-city {
+  float: right;
+  border-style: none;
+  color: #939aa9;
+  background-color: whitesmoke;
+  text-align: right;
+  background: #7db5ac;
+  width: 200px;
+  margin-top: 60px;
+}
