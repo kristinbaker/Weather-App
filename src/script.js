4e8fd36b7cbc545b73c5fc701af8a0ce48f63f11
kristinbaker <kristinbaker720@gmail.com>
2021-08-31 11:59:15 -0500

    implemented unit conversion links

--- src/script.js
+++ src/script.js
@@ -1,12 +1,15 @@
 let currentData = new Date();
+let apiKey = "f1b97e6818bf3a43bc9a1319c9ff238a";
+let units = `imperial`;
 let currentTime = document.querySelector("#current-time");
 let currentDate = document.querySelector("#current-date");
 let form = document.querySelector("#search-city-form");
 let searchButton = document.querySelector(".search-city-button");
 let celsiusLink = document.querySelector("#celsius-link");
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-let apiKey = "f1b97e6818bf3a43bc9a1319c9ff238a";
-let units = `imperial`;
+let currentTemp = document.querySelector(`#current-temp`);
+let currentHigh = document.querySelector(`#current-high`);
+let currentLow = document.querySelector(`#current-low`);
 let currentFahrenheitTemperature = null;
 let currentFahrenheitHigh = null;
 let currentFahrenheitLow = null;
@@ -79,10 +82,10 @@ let button = document.querySelector("#location-button");
 button.addEventListener("click", locationButton);
 
 function findLocation() {
-  navigator.geolocation.getCurrentPosition(updateCurrentLocation);
+  navigator.geolocation.getCurrentPosition(updateLocation);
 }
 
-function updateCurrentLocation(position) {
+function updateLocation(position) {
   let latitude = Math.round(position.coords.latitude);
   let longitude = Math.round(position.coords.longitude);
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
@@ -167,19 +170,18 @@ function formatDays(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
   let forecastElement = document.querySelector(`#forecast`);
   let forecast = response.data.daily;
   let forecastHTML = "";
   forecast.forEach(function (forecastDays, index) {
-    if (index < 5) {
+    if (index > 0 && index < 6) {
       forecastHTML =
         forecastHTML +
         `
     <div class="row-forecast">
-      <div class="col weather-forecast-date">${formatDays(
-        forecastDays.dt
-      )}</div>
+      <div class="col weather-forecast-date">${formatDays(forecastDays.dt)}
+
+      </div>
       <div class="col weather-forecast-emoji">
         <img
           src="http://openweathermap.org/img/wn/${
@@ -205,30 +207,23 @@ function displayForecast(response) {
 
 function convertToCelsius(event) {
   event.preventDefault();
-  let currentTemp = document.querySelector(`#current-temp`);
-  let currentHigh = document.querySelector(`#current-high`);
-  let currentLow = document.querySelector(`#current-low`);
-  let farenheit = document.querySelector(`#fahrenheit-link`);
-  let celsius = document.querySelector(`#celsius-link`);
-  farenheit.classList.remove("active");
-  celsius.classList.add("active");
-
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
   let currentCelsiusTemp = Math.round(
     (currentFahrenheitTemperature - 32) * (5 / 9)
   );
   let currentCelsiusHigh = Math.round((currentFahrenheitHigh - 32) * (5 / 9));
   let currentCelsiusLow = Math.round((currentFahrenheitLow - 32) * (5 / 9));
-  currentTemp.innerHTML = `${currentCelsiusTemp}`;
 
+  currentTemp.innerHTML = `${currentCelsiusTemp}`;
   currentHigh.innerHTML = `H:${currentCelsiusHigh}`;
   currentLow.innerHTML = `L:${currentCelsiusLow}`;
 }
 
 function convertToFahrenheit(event) {
   event.preventDefault();
-  let currentTemp = document.querySelector(`#current-temp`);
-  let currentHigh = document.querySelector(`#current-high`);
-  let currentLow = document.querySelector(`#current-low`);
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
   let updateFahrenheitHigh = Math.round(currentFahrenheitHigh);
   let updateFahrenheitLow = Math.round(currentFahrenheitLow);
 
